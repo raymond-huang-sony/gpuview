ae0ea9d6fcf7d2c7475718b06fb022625797ebbc
raymond-huang-sony <raymond.huang@sony.com>
2024-06-13 20:22:51 +0200

    debug

--- gpuview/app.py
+++ gpuview/app.py
@@ -29,7 +29,7 @@ EXCLUDE_SELF = False  # Do not report to `/gpustat` calls.
 def index():
     gpustats = core.all_gpustats()
     now = datetime.now().strftime('Updated at %Y-%m-%d %H-%M-%S')
-    return template(abs_views_path, gpustats=gpustats, update_time=now)
+    return template(os.path.realpath(__file__), gpustats=gpustats, update_time=now)
 
 
 @app.route('/gpustat', methods=['GET'])
